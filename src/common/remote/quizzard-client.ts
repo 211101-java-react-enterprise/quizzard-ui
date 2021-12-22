17202003ad6c4062ac1af215d985e7a820e40958
Wezley Singleton <wezley.singleton@gmail.com>
2021-12-22 00:51:14 -0500

    using deployed api endpoint

--- src/common/remote/quizzard-client.ts
+++ src/common/remote/quizzard-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const quizzardClient = axios.create({
-    baseURL: 'http://localhost:5000/quizzard',
+    baseURL: 'http://quizzardapi-dev.eba-puyqjfxg.us-east-1.elasticbeanstalk.com/quizzard',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
